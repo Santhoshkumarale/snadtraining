447730163e68a034af9eefc6ecc8dd4ebfb1c537
Santhoshkumarale <105612382+Santhoshkumarale@users.noreply.github.com>
2022-09-23 11:41:32 +0530

    question11
    question11

---
+++ Santhosh/assignment3/SNAD EXAM/assessment3 que11/assessment3 que11/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace assessment3_que11
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=SANTHOSH12345\\SQLEXPRESS;Initial Catalog=SnadDB;Integrated Security=True;"))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand("select * from Employee", con))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.WriteLine(reader.GetValue(i));
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
